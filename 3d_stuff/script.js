3f25477688b28bae2a2231c7af0ed0b067467743
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 07:20:46 +0000

    add execute permission to bin/deploy [ci skip]

--- 3d_stuff/script.js
+++ 3d_stuff/script.js
@@ -1,13 +1,1 @@
-// Handles loading the events for <model-viewer>'s slotted progress bar
-const onProgress = (event) => {
-  const progressBar = event.target.querySelector('.progress-bar');
-  const updatingBar = event.target.querySelector('.update-bar');
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-  if (event.detail.totalProgress === 1) {
-    progressBar.classList.add('hide');
-    event.target.removeEventListener('progress', onProgress);
-  } else {
-    progressBar.classList.remove('hide');
-  }
-};
-document.querySelector('model-viewer').addEventListener('progress', onProgress);
+const onProgress=e=>{const r=e.target.querySelector(".progress-bar");e.target.querySelector(".update-bar").style.width=`${100*e.detail.totalProgress}%`,1===e.detail.totalProgress?(r.classList.add("hide"),e.target.removeEventListener("progress",onProgress)):r.classList.remove("hide")};document.querySelector("model-viewer").addEventListener("progress",onProgress);
